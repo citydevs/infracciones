8df93a6851b2b3bf1426e0c6d0b8088a507b7d3d
mikesaurio <mikesaurio@gmail.com>
2015-03-07 18:54:20 -0600

    se agrega pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150307232104) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "infractions", force: :cascade do |t|
     t.text     "nombre"
     t.text     "placa"
