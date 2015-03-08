0d221b807d2ef7e413674774799a0692e4c678c0
mikesaurio <mikesaurio@gmail.com>
2015-03-07 23:28:18 -0600

    se eliminan columnas que no se usan

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308033427) do
+ActiveRecord::Schema.define(version: 20150308051831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,18 +27,8 @@ ActiveRecord::Schema.define(version: 20150308033427) do
   create_table "infractions", force: :cascade do |t|
     t.text     "nombre"
     t.text     "placa"
-    t.integer  "raking_honestidad"
-    t.integer  "raking_trato"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.text     "identificacion"
-    t.text     "infraccion"
-    t.text     "articulo"
-    t.text     "sancion"
-    t.text     "devolvio"
-    t.text     "copia"
-    t.float    "latitud"
-    t.float    "longitud"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
